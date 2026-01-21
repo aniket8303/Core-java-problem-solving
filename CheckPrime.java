ab9498989560946db4488b0bbbfba5d3592f4902
Aniket Parekar <parekaraniket25@gmail.com>
2026-01-21 22:26:42 +0530

    Create CheckPrime.java

---
+++ CheckPrime.java
@@ -0,0 +1,39 @@
+// Given an integer n check if n is prime or not.
+// A prime number is a number that is divisible by 1 and itself only.
+
+// Note: Print "True" if n is prime, otherwise print "False".
+
+// Examples:
+
+// Input: n = 17
+// Output: True 
+// Explanation: 17 is  divisible by only 1 and 17. So it's a prime number.
+// Input: n = 56
+// Output: False
+// Explanation: 56 is divisible by 2, 4, 7.....etc. So its not a prime number.
+// Contraints:
+// 1 <= n <= 104
+
+
+import java.util.*;
+class Solution {
+  public static void main(String[]args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int count = 0;
+    for(int i = 1; i <= n; i++){
+      if(n%i==0){
+        count++;
+      }
+    }
+    if(count==2){
+      System.out.println("True");
+    }
+    else{
+      System.out.println("False");
+    }
+  }
+}
+
+
+
