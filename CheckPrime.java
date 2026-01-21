38f66cce3612bb42d72cceb2e6d0a44247d37865
Aniket Parekar <parekaraniket25@gmail.com>
2026-01-21 22:27:44 +0530

    Fix formatting issue in CheckPrime.java

--- CheckPrime.java
+++ CheckPrime.java
@@ -20,7 +20,7 @@ class Solution {
   public static void main(String[]args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
-    int count = 0;
+    int count = 0; 
     for(int i = 1; i <= n; i++){
       if(n%i==0){
         count++;
