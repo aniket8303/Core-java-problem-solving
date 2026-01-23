1af412cf060e11c7c6d3bbee3975d2cc579dacee
Aniket Parekar <parekaraniket25@gmail.com>
2026-01-23 19:33:05 +0530

    Create  AnonymousInnerClass1.java

---
+++ InterfaceExample/InnerClass/AnonymousInnerClass1.java
@@ -0,0 +1,22 @@
+@FunctionalInterface
+interface Demo{
+	void m1();
+}
+class AnonymousInnerClass{
+	public static void main(String[] args) {
+		Demo obj = new Demo(){
+			@Override
+			public void m1(){
+				System.out.println("m1() implemented");
+			}
+		};
+		obj.m1();
+	}
+}
+
+//OUTPUT
+
+// D:\java>javac AnonymousInnerClass.java
+
+// D:\java>java AnonymousInnerClass
+// m1() implemented
