349c68461c4f4b714aaf9e73b520b8a12e166fe2
Aniket Parekar <parekaraniket25@gmail.com>
2026-01-30 18:49:37 +0530

    Create CheckedException1.java

---
+++ Exception Handling/CheckedException1.java
@@ -0,0 +1,39 @@
+//CheckedException
+//1.Throwable
+
+//first way of handling exception  i.e try-catch
+
+// class CheckedException1{
+// 	public static void main(String[] args) {
+// 		System.out.println("Hello");
+// 		try{
+// 			m1();
+// 		}catch(Throwable e){
+// 			System.out.println("Exception handled");
+// 		}
+// 		System.out.println("Java");
+// 	}
+// 	public static void m1() throws Throwable{
+//         System.out.println("Hello i am m1()");
+// 	}
+// }
+
+//Second ways of handling exception i.e. using throws
+class CheckedException1{
+	public static void main(String[] args) throws Throwable {
+		System.out.println("Hello");
+		m1();
+		System.out.println("Java");
+	}
+	public static void m1() throws Throwable{
+        System.out.println("Hello i am m1()");
+	}
+}
+
+// OUTPUT
+// D:\java\Exception-Handling>javac CheckedException1.java
+
+// D:\java\Exception-Handling>java CheckedException1
+// Hello
+// Hello i am m1()
+// Java
