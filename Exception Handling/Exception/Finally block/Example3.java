8715c7b15d78dfcd700e820711487ac4db7297f2
Aniket Parekar <parekaraniket25@gmail.com>
2026-02-03 17:27:53 +0530

    Create Example3.java

---
+++ Exception Handling/Exception/Finally block/Example3.java
@@ -0,0 +1,26 @@
+class Example1{
+	public static void main(String[] args) {
+		System.out.println("Starts");
+		try{
+			System.out.println("try");
+			System.out.println(10%0);
+		}
+		catch(NullPointerException e){
+			System.out.println("catch");
+		}
+		finally{
+			System.out.println("finally");
+		}
+		System.out.println("ends");
+	}
+}
+
+//OUTPUT
+// D:\java\Exception-Handling>javac Example1.java
+
+// D:\java\Exception-Handling>java Example1
+// Starts
+// try
+// finally
+// Exception in thread "main" java.lang.ArithmeticException: / by zero
+//         at Example1.main(Example1.java:39)
