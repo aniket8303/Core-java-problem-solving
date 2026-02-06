189f811c4a3979d81eef3ae87186471345a9e7e2
Aniket Parekar <parekaraniket25@gmail.com>
2026-02-06 14:27:32 +0530

    Create ExceptionExample2.java

---
+++ Exception Handling/Exception/Methods-Of-Throwable/ExceptionExample2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class ExceptionExample2{
+	public static void main(String[] args) {
+		Stack stack = new Stack();
+		try{
+			System.out.println(stack.pop());
+		}
+		catch(EmptyStackException e){
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+// //OUTPUT
+// D:\java\Exception-Handling>javac ExceptionExample2.java
+
+// D:\java\Exception-Handling>java ExceptionExample2
+// null
